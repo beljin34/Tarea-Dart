40f5853ef09dfbaa61d619ff10d6cb96dd1a37e5
Isabel Condori <manitaskookie@gmail.com>
2026-03-17 00:01:28 -0400

    Create Ejercicio12.dart

---
+++ Ejercicio12.dart
@@ -0,0 +1,10 @@
+void main() {
+  List<int> numeros = [1, 2, 3, 4];
+  int buscar = 3;
+
+  if (numeros.contains(buscar)) {
+    print("Encontrado");
+  } else {
+    print("No encontrado");
+  }
+}
